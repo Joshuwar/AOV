991b8f88220f3331d0a75e0372c39d971645de78
Joshua <josh.u.war@gmail.com>
2012-08-09 09:29:04 +0100

    Removing 404 emailer

--- 404.php
+++ 404.php
@@ -7,13 +7,4 @@
 			<p>Sorry! The page you were looking for does not exist. We've been notified about this. Please go back to the <a href="/">home page</a> and continue looking around.</p>
 		</div>
 	<br class="clearboth marginbottom" />
-	<?php $adminemail = get_option('admin_email');
-		$websitename = get_bloginfo('name');
-		echo $website;
-		$message = "A user tried to go to ".$_SERVER['REQUEST_URI']
-			." and received a 404 (page not found) error. "
-	        ."They came from ".$_SERVER['HTTP_REFERER'];
-		wp_mail($adminemail, "Bad Link To ".$_SERVER['REQUEST_URI'],
-	        $message, "From: BPSF no-reply <noreply@bellpottinger-sansfrontieres.com>");
-	?>
 <?php get_footer(); ?>
